0144c1c256540fb5467653d53de23dde1d679a8b
= <peterilf@au1.ibm.com>
2015-10-10 17:24:59 +1100

    Hacked the geo location

--- client/views/dashboard/dashboard.js
+++ client/views/dashboard/dashboard.js
@@ -5,71 +5,75 @@ var toRadians=function(Value){
 
 }
 var getClosestLocation=function(){
-    var closest=null;
-    var dist=null;
-
-    _getDistance=function(me,test){
-        var R = 6371000; // metres
-        var φ1 = toRadians(me.lat);
-        var φ2 = toRadians(test.location.latitude);
-        var Δφ = toRadians(test.location.latitude-me.lat);
-        var Δλ = toRadians(test.location.longitude-me.lng);
-
-        var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
-            Math.cos(φ1) * Math.cos(φ2) *
-            Math.sin(Δλ/2) * Math.sin(Δλ/2);
-        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-
-        var d = R * c;
-       // console.log(d);
-        return d;
-
-    }
-    var arenas=dbArena.find().fetch();
-    //console.log('arenas',arenas);
-
-
-
-
-
-
-
-    var me=userGeoLocation.get();
-    //console.log(me);
-    if(me==null||me==undefined){
-        console.log('gps was undefined');
-        me={
-            lat:-38,
-            lng:144.991
-        }
-    }
-
-    var closest=null;
-    arenas.forEach(function(test){
-        var tempDist=_getDistance(me,test);
-        if(dist==null||dist>tempDist){
-            dist=tempDist;
-            closest=test;
-        }
-
-    })
-    if (closest !== null) {
-        console.log('you are closer to '+closest.name+' and are distant '+dist+' metres');
-    }
-    Session.set('distance',dist);
-    Session.set('closest',closest);
-
-
 
 }
 
 
 Template.dashboard.helpers({
-    minDistance:function(){
-        var d=Session.get('distance');
-        if(d<100){
+    minDistance:function() {
+        var closest=null;
+        var dist=null;
+
+        _getDistance=function(me,test){
+            var R = 6371000; // metres
+            var φ1 = toRadians(me.lat);
+            var φ2 = toRadians(test.location.latitude);
+            var Δφ = toRadians(test.location.latitude-me.lat);
+            var Δλ = toRadians(test.location.longitude-me.lng);
+
+            var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+                Math.cos(φ1) * Math.cos(φ2) *
+                Math.sin(Δλ/2) * Math.sin(Δλ/2);
+            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+            var d = R * c;
+            // console.log(d);
+            return d;
+
+        }
+        var arenas=dbArena.find().fetch();
+        //console.log('arenas',arenas);
+
+
+
+
+
+
+
+        var me=userGeoLocation.get();
+        //console.log(me);
+        if(me==null||me==undefined){
+            console.log('gps was undefined');
+            me={
+                lat:-38,
+                lng:144.991
+            }
+        }
+
+        var closest=null;
+        arenas.forEach(function(test){
+            var tempDist=_getDistance(me,test);
+            if(dist==null||dist>tempDist){
+                dist=tempDist;
+                closest=test;
+            }
+
+        })
+        if (closest !== null) {
+            console.log('you are closer to '+closest.name+' and are distant '+dist+' metres');
+        }
+        else {
+            closest = dbArena.find().fetch()[0];
+            dist = 2;
+        }
+        Session.set('distance',dist);
+        Session.set('closest',closest);
+
+        var d = Session.get('distance');
+        if(d < 100) {
             return true;
-        }else{
+        }
+        else {
             return false;
         }
     },
@@ -85,5 +89,5 @@ Template.dashboard.rendered=function(){
     //console.log(userGeoLocation.get());
     var arenas=dbArena.find().fetch();
     //console.log('arenas',arenas);
-    getClosestLocation();
+    //    getClosestLocation();
 }
