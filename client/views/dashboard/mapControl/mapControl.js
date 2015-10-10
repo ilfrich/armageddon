461dd85e07c16152f506d66fca8e95e864292291
= <peterilf@au1.ibm.com>
2015-10-10 16:57:11 +1100

    Formatted mapcontrol code;

--- client/views/dashboard/mapControl/mapControl.js
+++ client/views/dashboard/mapControl/mapControl.js
@@ -1,13 +1,13 @@
-var short=[];
-var medium=[];
-var long=[];
-var offers=[];
-var needs=[];
-var waiting=[];
+var short = [];
+var medium = [];
+var long = [];
+var offers = [];
+var needs = [];
+var waiting = [];
 
 
-var matchIcon=function(item){
-    switch (item){
+var matchIcon = function (item) {
+    switch (item) {
         case "Accomodation":
             return 'bed';
             break;
@@ -28,70 +28,84 @@ var matchIcon=function(item){
             break;
 
 
-
     }
 
 
-}
+};
 
 Template.mapControl.helpers({
-    mapOptions: function() {
-        var geo=userGeoLocation.get();
-        if(geo==null){
-            geo={
-                lat:-38,
-                lng:144.991
+    mapOptions: function () {
+        var geo = userGeoLocation.get();
+        if (geo == null) {
+            geo = {
+                lat: -38,
+                lng: 144.991
             }
         }
         if (GoogleMaps.loaded()) {
             return {
                 center: new google.maps.LatLng(geo.lat, geo.lng),
                 zoom: 10,
-                styles:[{"featureType":"landscape","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"stylers":[{"hue":"#00aaff"},{"saturation":-100},{"gamma":2.15},{"lightness":12}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"visibility":"on"},{"lightness":24}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":57}]}]
+                styles: [{
+                    "featureType": "landscape",
+                    "elementType": "labels",
+                    "stylers": [{"visibility": "off"}]
+                }, {
+                    "featureType": "transit",
+                    "elementType": "labels",
+                    "stylers": [{"visibility": "off"}]
+                }, {
+                    "featureType": "poi",
+                    "elementType": "labels",
+                    "stylers": [{"visibility": "off"}]
+                }, {
+                    "featureType": "water",
+                    "elementType": "labels",
+                    "stylers": [{"visibility": "off"}]
+                }, {
+                    "featureType": "road",
+                    "elementType": "labels.icon",
+                    "stylers": [{"visibility": "off"}]
+                }, {"stylers": [{"hue": "#00aaff"}, {"saturation": -100}, {"gamma": 2.15}, {"lightness": 12}]}, {
+                    "featureType": "road",
+                    "elementType": "labels.text.fill",
+                    "stylers": [{"visibility": "on"}, {"lightness": 24}]
+                }, {"featureType": "road", "elementType": "geometry", "stylers": [{"lightness": 57}]}]
             };
         }
     },
 });
 
 
+Template.mapControl.rendered = function () {
 
-Template.mapControl.rendered=function(){
+};
 
-}
-
-Template.mapControl.onCreated(function() {
-    GoogleMaps.ready('map', function(map) {
-
+Template.mapControl.onCreated(function () {
+    GoogleMaps.ready('map', function (map) {
 
 
-        google.maps.event.addListener(map.instance, 'click', function(event) {
+        google.maps.event.addListener(map.instance, 'click', function (event) {
             //Markers.insert({ lat: event.latLng.lat(), lng: event.latLng.lng() });
         });
-
-
-
-
-
 
 
         var markers = {};
 
         dbArena.find().observe({
-         added: function(document) {
+            added: function (document) {
 
-         // Create a marker for this document
+                // Create a marker for this document
 
-          //   var icon=matchIcon(document.category);
-             /*console.log(document.category);
-             console.log(do)
-             console.log(icon);
-*/
-  //           var image = 'images/markers/azure.png';
-
-
+                //   var icon=matchIcon(document.category);
+                /*console.log(document.category);
+                 console.log(do)
+                 console.log(icon);
+                 */
+                //           var image = 'images/markers/azure.png';
 
 
-          /*   var contentString = '<div id="content">'+
+                /*   var contentString = '<div id="content">'+
                  '<div id="siteNotice">'+
                  '</div>'+
                  '<h1 id="firstHeading" class="firstHeading"><i class="fa fa-'+icon+'" ></i><span style="margin-left:20px">'+document.item+'</span>    </h1>'+
@@ -102,37 +116,26 @@ Template.mapControl.onCreated(function() {
                  '<p>Attribution: Match, <a href="">'+document.date+'</p>'+
                  '</div>'+
                  '</div>';
-             var infowindow = new google.maps.InfoWindow({
+                 var infowindow = new google.maps.InfoWindow({
                  content: contentString
-             });*/
+                 });*/
 
-             console.log(document)
+                console.log(document);
 
-             var markerOffer = new google.maps.Marker({
+                var markerOffer = new google.maps.Marker({
 
-                animation: google.maps.Animation.DROP,
-                position: new google.maps.LatLng(document.location.latitude, document.location.longitude),
-                map: map.instance,
-                title: document.name,
-         // We store the document _id on the marker in order
-         // to update the document within the 'dragend' event belo¸¸w.
-                id:document._id, visible:true
-            });
-
-
+                    animation: google.maps.Animation.DROP,
+                    position: new google.maps.LatLng(document.location.latitude, document.location.longitude),
+                    map: map.instance,
+                    title: document.name,
+                    // We store the document _id on the marker in order
+                    // to update the document within the 'dragend' event belo¸¸w.
+                    id: document._id, visible: true
+                });
 
 
-
-
-
-
-
-         }
-         });
-
-
-
-
+            }
+        });
 
 
     });
