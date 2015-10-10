43dd361bf75c331c3b351289f937153d4b2ec695
Alessio Bonti <alebonti@au1.ibm.com>
2015-10-10 23:36:19 +1100

    hey im tired :D

--- client/views/lobbies/arenalobby/arenalobby.js
+++ client/views/lobbies/arenalobby/arenalobby.js
@@ -1,4 +1,26 @@
 Template.arenalobby.helpers({
+    king:function() {
+        //console.log(this.arena);
+        ranks=this.arena.ranking.sort(function(a,b){
+            return b.points- a.points;
+        });
+        console.log(ranks);
+        var result;
+        result=Meteor.call('fetchUserProfile', ranks[0].userId, function(err, data){
+           Session.set('user',data);
+        });
+        return Session.get('user');
+
+    },
+    highScore:function(){
+        ranks=this.arena.ranking.sort(function(a,b){
+            return b.points- a.points;
+        });
+        return ranks[0];
+
+    }
+
+
 
 });
 
@@ -7,5 +29,7 @@ Template.arenalobby.events({
 });
 
 Template.arenalobby.rendered=function(){
+
+
 
 }
